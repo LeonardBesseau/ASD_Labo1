744b5ebe9e812752a2f86d7cdbf4762910f44d73
LeonardBesseau <leonard.besseau@heig-vd.ch>
2020-02-28 14:32:19 +0100

    corrected code

--- main.cpp
+++ main.cpp
@@ -31,14 +31,14 @@ using namespace std;
  *
  * @param v vector dans lequel on cherche
  * @param val valeur à chercher
- * @return la position de la valeur dans le vector si trouvé, -1 sinon
+ * @return le nombre d'itération effectué
  */
 size_t chercherPosition(const vector<int> &v, int val) {
     for (size_t i = 0; i < v.size(); ++i) {
         if (v.at(i) == val)
             return i;
     }
-    return -1;
+    return v.size();
 }
 
 
@@ -48,17 +48,20 @@ size_t chercherPosition(const vector<int> &v, int val) {
  * Pour la complexité, on considère le nombre de comparaisons (>)
  *
  * @param v vector à trier
+ * @return le nombre de comparaison effectué
  */
-void trier(vector<int> &v) {
-    if (v.empty()) return;
-
+int trier(vector<int> &v) {
+    if (v.empty()) return 0;
+    int output = 0;
     for (int a : v) {
         auto j = v.begin();
         auto k = j++;
         for (; j != v.end(); k = j++)
-            if (*k > *j)
+            if (++output, *k > *j)
                 swap(*k, *j);
     }
+    return output;
+
 }
 
 /**
@@ -70,23 +73,24 @@ void trier(vector<int> &v) {
  *
  * @param v vector trié dans lequel on cherche
  * @param val valeur à chercher
- * Retourne true si la valeur est contenue dans le vector, false sinon.
+ * @return le nombre d'iteration
  */
-bool chercherSiContient(const vector<int> &v, int val) {
+size_t chercherSiContient(const vector<int> &v, int val) {
     auto first = v.begin();
     auto last = v.end();
-
+    size_t output = 0;
     while (first != last) {
         auto mid = first + (last - first) / 2;
+        ++output;
         if (*mid == val) {
-            return true;
+            return output;
         } else if (*mid < val) {
             first = mid + 1;
         } else {
             last = mid;
         }
     }
-    return false;
+    return output;
 }
 
 /**
@@ -106,13 +110,17 @@ unsigned long long f(unsigned n) {
  * 5. Pour la complexité, on considère le nombre d'additions (+=)
  *
  * @param v
+ * @return le nombre d'addition
  */
-void g(vector<int> &v) {
+size_t g(vector<int> &v) {
+    size_t output = 0;
     for (size_t i = 0; i < v.size(); ++i) {
         for (size_t j = v.size() - 1; j > 0; j /= 2) {
+            ++output;
             v.at(i) += v.at(j);
         }
     }
+    return output;
 }
 
 /**
@@ -153,90 +161,133 @@ vector<int> random2(size_t N, int maxVal) {
     return v;
 }
 
-void test1(){
+/**
+ * Test pour mesurer la complexité de la fonction 1 (nb d'itération) en moyenne
+ * @param nbPoint nombre de point à tester
+ * @return un vecteur avec les données
+ */
+vector<double> test1(int nbPoint) {
     std::vector<int> point;
-    std::vector<vector<int>> list;
-    std::vector<double> timer;
-    const int nbInst = 20000;
-    timer.resize(nbInst);
-    point.resize(nbInst);
-    list.resize(nbInst);
-    for (int i = 0; i < nbInst; ++i) {
-        point.at(i) = i;
-        list.at(i).resize(i);
-        for (int j = 0; j < i; ++j) {
-            list.at(i).at(j)= j;
+    std::vector<double> output;
+    output.resize(nbPoint);
+    for (int i = 0; i < nbPoint; ++i) {
+        point.push_back(i);
+        size_t moyenne = 0;
+        cout << "iteration " << i << endl;
+        // moyenne pour vecteur de taille i
+        for (int j = 0; j < point.size(); ++j) {
+            moyenne += chercherPosition(point, j);
         }
+        output.at(i) = moyenne / point.size();
     }
-
-    for (int a : point) {
-        chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
-        chercherPosition(list.at(a), 7);
-        chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
-        timer.at(a) = chrono::duration_cast<chrono::nanoseconds>(
-                t2 - t1).count();
+    for (double a : output) {
+        cout << a << endl;
     }
-    for (int a : point) {
-        std::cout << a << " : " << timer.at(a) << endl;
-    }
+    return output;
 }
 
-void test4(){
+/** *
+ * Test pour mesurer la complexité de la fonction 2 (nb de comparaison)
+ * @param nbPoint nombre de point à tester
+ * @return un vecteur avec les données
+ */
+vector<double> test2(int nbPoint) {
     std::vector<int> point;
-    std::vector<double> timer;
-    const int nbInst = 20;
-    timer.resize(nbInst);
-    point.resize(nbInst);
-    for (int i = 0; i < 20; ++i) {
-        point.at(i) = i;
+    std::vector<double> output;
+    output.resize(nbPoint);
+
+    for (int i = 0; i < nbPoint; ++i) {
+        point.push_back(i);
+        cout << "iteration " << i << endl;
+        output.at(i) = trier(point);
+
     }
 
-    for (int a : point) {
-        cout << "Point " << a << endl;
-        chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
-        f(a);
-        chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
-        timer.at(a) = chrono::duration_cast<chrono::nanoseconds>(
-                t2 - t1).count();
+    for (int j = 0; j < nbPoint; ++j) {
+        cout << j << " " << output.at(j) << endl;
     }
-    for (int a : point) {
-        std::cout << a << " : " << timer.at(a) << endl;
-    }
+
+    return output;
 }
 
-void test5(){
+/** *
+ * Test pour mesurer la complexité de la fonction 3 (nb d'itération) en moyenne
+ * @param nbPoint nombre de point à tester
+ * @return un vecteur avec les données
+ */
+vector<double> test3(int nbPoint) {
     std::vector<int> point;
-    std::vector<vector<int>> list;
-    std::vector<double> timer;
-    const int nbInst = 200;
-    timer.resize(nbInst);
-    point.resize(nbInst);
-    list.resize(nbInst);
-    for (int i = 0; i < nbInst; ++i) {
-        point.at(i) = i;
-        list.at(i).resize(i);
-        for (int j = 0; j < i; ++j) {
-            list.at(i).at(j)= j;
+    std::vector<double> output;
+    output.resize(nbPoint);
+    for (int i = 0; i < nbPoint; ++i) {
+        point.push_back(i);
+        size_t moyenne = 0;
+        cout << "iteration " << i << endl;
+        for (int j = 0; j < point.size(); ++j) {
+            moyenne += chercherSiContient(point, j);
         }
+        output.at(i) = i ? moyenne / point.size() : moyenne;
     }
-
-    for (int a : point) {
-        chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
-        g(list.at(a));
-        chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
-        timer.at(a) = chrono::duration_cast<chrono::nanoseconds>(
-                t2 - t1).count();
+    for (int k = 0; k < nbPoint; ++k) {
+        cout << k << " " << output.at(k) << endl;
     }
-    for (int a : point) {
-        std::cout << a << " : " << timer.at(a) << endl;
-    }
+    return output;
 }
 
+/** *
+ * Test pour mesurer la complexité de la fonction 4
+ * @param nbPoint nombre de point à tester
+ * @return un vecteur avec les données
+ */
+vector<double> test4(int nbPoint) {
+    std::vector<double> output;
+    output.resize(nbPoint);
 
-void test6(){
+    for (int i = 0; i < nbPoint; ++i) {
+        cout << i << endl;
+        output.at(i) = f(i);
+
+    }
+
+    for (int j = 0; j < nbPoint; ++j) {
+        cout << j << " " << output.at(j) << endl;
+    }
+
+    return output;
+}
+
+/** *
+ * Test pour mesurer la complexité de la fonction 5 (nb d'addition) en moyenne
+ * @param nbPoint nombre de point à tester
+ * @return un vecteur avec les données
+ */
+vector<double> test5(int nbPoint) {
+    std::vector<int> point;
+    std::vector<double> output;
+    output.resize(nbPoint);
+
+    for (int i = 0; i < nbPoint; ++i) {
+        point.push_back(i);
+        cout << i << endl;
+        output.at(i) = g(point);
+
+    }
+
+    for (int j = 0; j < nbPoint; ++j) {
+        cout << j << " " << output.at(j) << endl;
+    }
+
+    return output;
+}
+
+/** *
+ * Test pour mesurer le temps d'execution de la fonction 6
+ * @param nbPoint nombre de point à tester
+ * @return un vecteur avec les données
+ */
+vector<double> test6(int nbInst) {
     std::vector<int> point;
     std::vector<double> timer;
-    const int nbInst = 200;
     timer.resize(nbInst);
     point.resize(nbInst);
     for (int i = 0; i < nbInst; ++i) {
@@ -244,22 +295,47 @@ void test6(){
     }
 
     for (int a : point) {
-        cout << "Point " << a << endl;
         chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
         random(a, 256);
         chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
         timer.at(a) = chrono::duration_cast<chrono::nanoseconds>(
                 t2 - t1).count();
-    }
-    for (int a : point) {
         std::cout << a << " : " << timer.at(a) << endl;
     }
+
+    return timer;
+}
+
+/** *
+ * Test pour mesurer le temps d'execution de la fonction 7
+ * @param nbPoint nombre de point à tester
+ * @return un vecteur avec les données
+ */
+vector<double> test7(int nbInst) {
+    std::vector<int> point;
+    std::vector<double> timer;
+    timer.resize(nbInst);
+    point.resize(nbInst);
+    for (int i = 0; i < nbInst; ++i) {
+        point.at(i) = i;
+    }
+
+    for (int a : point) {
+        chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
+        random2(a, 256);
+        chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
+        timer.at(a) = chrono::duration_cast<chrono::nanoseconds>(
+                t2 - t1).count();
+        std::cout << a << " : " << timer.at(a) << endl;
+    }
+
+    return timer;
 }
 
 int main() {
     //initialisation du générateur aléatoire
     srand(time(NULL));
-    cout << f(2);
+    test1(1000);
 
 
 }
